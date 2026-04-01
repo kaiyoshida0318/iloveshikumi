e62120da98be2269e677ce10e64b290546710d69
kaiyoshida0318 <kaiyoshida0318@gmail.com>
2026-04-01 15:30:54 +0900

    Add KWvol(CL/CTR*100) and KWshare(vol/totalVol) calculation

--- app.js
+++ app.js
@@ -465,12 +465,43 @@ async function kanriRun() {
       var co=kwI.slice().sort(function(a,b){return _parseNum(b['実績額(合計)']||0)-_parseNum(a['実績額(合計)']||0);});
       function eb(bl,so){
         var x=newRow();setF(x,bl,'------------------------------');outRows.push(x);
-        var y=newRow();setF(y,bl,'広告合計-③',{click:iCl||'',cost:iCo||'',sales:iSa||'',units:iUn||'',avg_cpc:_calcAvgCpc(iCo,iCl),cvr:_calcCvr(iUn,iCl),roas:_calcRoas(iSa,iCo)});outRows.push(y);
-        if(itI.length){var z=newRow();setF(z,bl,'商品CPC(20円出稿分)-④',{click:dCl||'',cost:dCo||'',sales:dSa||'',units:dUn||'',avg_cpc:_calcAvgCpc(dCo,dCl),cvr:_calcCvr(dUn,dCl),roas:_calcRoas(dSa,dCo)});outRows.push(z);}
-        var w=newRow();setF(w,bl,'KW合計(下記KWの合計)-④',{click:kCl||'',cost:kCo||'',sales:kSa||'',units:kUn||'',avg_cpc:_calcAvgCpc(kCo,kCl),cvr:_calcCvr(kUn,kCl),roas:_calcRoas(kSa,kCo)});outRows.push(w);
-        so.forEach(function(kw){var c1=_parseNum(kw['クリック数(合計)']||0),c2=_parseNum(kw['実績額(合計)']||0),s1=_parseNum(kw['売上金額(合計720時間)']||0),u1=_parseNum(kw['売上件数(合計720時間)']||0);var rk=newRow();setF(rk,bl,kw['キーワード']||'',{ctr:kw['CTR(%)']||'',click:c1||'',cost:c2||'',sales:s1||'',units:u1||'',avg_cpc:_calcAvgCpc(c2,c1),target_cpc:kw['目安CPC']||'',cvr:_calcCvr(u1,c1),roas:_calcRoas(s1,c2)});outRows.push(rk);});
+        var y=newRow();setF(y,bl,'広告合計-④',{click:iCl||'',cost:iCo||'',sales:iSa||'',units:iUn||'',avg_cpc:_calcAvgCpc(iCo,iCl),cvr:_calcCvr(iUn,iCl),roas:_calcRoas(iSa,iCo)});outRows.push(y);
+        if(itI.length){var z=newRow();setF(z,bl,'商品CPC(20円出謔分)-⑤',{click:dCl||'',cost:dCo||'',sales:dSa||'',units:dUn||'',avg_cpc:_calcAvgCpc(dCo,dCl),cvr:_calcCvr(dUn,dCl),roas:_calcRoas(dSa,dCo)});outRows.push(z);}
+        var w=newRow();setF(w,bl,'KW合計(下記KWの合計)-⑤',{click:kCl||'',cost:kCo||'',sales:kSa||'',units:kUn||'',avg_cpc:_calcAvgCpc(kCo,kCl),cvr:_calcCvr(kUn,kCl),roas:_calcRoas(kSa,kCo)});outRows.push(w);
+        // KWvol合計を先に計算（KWシェア算出に必要）
+        var totalVol=0;
+        so.forEach(function(kw){
+          var cl=_parseNum(kw['クリック数(合計)']||0);
+          var ctr=_parseNum(kw['CTR(%)']||0);
+          var vol=ctr>0?Math.round(cl/ctr*100):0;
+          totalVol+=vol;
+        });
+        so.forEach(function(kw){
+          var c1=_parseNum(kw['クリック数(合計)']||0);
+          var c2=_parseNum(kw['実績額(合計)']||0);
+          var s1=_parseNum(kw['売上金額(合計720時間)']||0);
+          var u1=_parseNum(kw['売上件数(合計720時間)']||0);
+          var ctr=_parseNum(kw['CTR(%)']||0);
+          var kwvol=ctr>0?Math.round(c1/ctr*100):0;
+          var kwshare=totalVol>0?Math.round(kwvol/totalVol*10000)/100:0;
+          var rk=newRow();
+          setF(rk,bl,kw['キーワード']||'',{
+            kwvol:kwvol||'',
+            share:kwshare||'',
+            ctr:kw['CTR(%)']||'',
+            click:c1||'',
+            cost:c2||'',
+            sales:s1||'',
+            units:u1||'',
+            avg_cpc:_calcAvgCpc(c2,c1),
+            target_cpc:kw['目安CPC']||'',
+            cvr:_calcCvr(u1,c1),
+            roas:_calcRoas(s1,c2)
+          });
+          outRows.push(rk);
+        });
         var x2=newRow();setF(x2,bl,'------------------------------');outRows.push(x2);
-        if(cpn){var r5=newRow();setF(r5,bl,'クーポンアドバンス-⑤',{cost:cpn.cost||'',sales:cpn.sales||'',roas:_calcRoas(cpn.sales,cpn.cost)});outRows.push(r5);}
+        if(cpn){var r5=newRow();setF(r5,bl,'クーポンアドバンス-⑥',{cost:cpn.cost||'',sales:cpn.sales||'',roas:_calcRoas(cpn.sales,cpn.cost)});outRows.push(r5);}
       }
       eb('☆',sh);eb('★',co);
     });
